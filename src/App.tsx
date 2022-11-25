575c526512d7578191e409550cff1fc9a9168c9c
Radjan Bayhombaev <rbayhombaev@gmail.com>
2022-11-25 14:15:55 +0600

    Сделал логику вывода стран Также добавил в Стейт границы

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './App.css'
+
+interface Country { 
+  name: string;
+  alpha3Code: string;
+}
 
 function App() {
+  const UrlWithName = 'https://restcountries.com/v2/all?fields=alpha3Code,name'
+  const UrlWithcode = 'https://restcountries.com/v2/alpha/'
+
+  const [name, setName] = useState<Country[]>([])
+
+  useEffect(() => {
+    const setCountry = async () => {
+      const responseArr= await axios.get<Country[]>(UrlWithName)
+  
+      const promises = responseArr.data.map(async country => {
+        setName(prev => [...prev , country])
+      })
+    }
+
+    setCountry()
+  }, [])
+
+  const enterToConsole = async (code: string) => {
+    const responseCode = await axios.get(UrlWithcode + code)
+
+    const borders = responseCode.data.borders
+
+    if (borders){
+      for (let i = 0; i < borders.length; i++){
+        const countryBorders = await axios.get(UrlWithcode + borders[i])
+  
+        console.log(countryBorders.data.name)
+      }
+    }else {
+      console.log('this country has no borders')
+    }
+
+  } 
+
+  const createCountrys = name.map( createCountry => {
+   return  <div key={createCountry.alpha3Code}>
+      <p onClick={() => enterToConsole(createCountry.alpha3Code)}>{createCountry.name}</p>
+    </div>
+  })
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {createCountrys}
+      <div>qweqwe</div>
     </div>
   );
 }
